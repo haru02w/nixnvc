398c042ddd837af1c799afa59afd8ad7007d746e
Haru02w <haru02w@protonmail.com>
2024-01-02 10:49:29 -0300

    nix syntax highlight too slow

--- nvim/plugin/treesitter.lua
+++ nvim/plugin/treesitter.lua
@@ -1,6 +1,9 @@
 require'nvim-treesitter.configs'.setup {
   highlight = {
     enable = true,
+    disable = {
+      'nix', -- too slow
+    },
     additional_vim_regex_highlighting = false,
   },
 }
